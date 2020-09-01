af0faac2fd49194b8eedfec73bb288bd63e9e684
Sonu Kumar <sonu0774@gmail.com>
2020-09-01 20:26:55 +0530

    Changes in product less= 0

--- src/components/Category/CategoryGridTwo.js
+++ src/components/Category/CategoryGridTwo.js
@@ -54,80 +54,84 @@ const CategoryGridTwo = ({ spaceBottomClass, categoryData, productData , firstCa
         
           {categoryData.map((category, index) => {
             // const data = category["_id"];
-           
+           const productOnline = productData.filter(product => product.supplierCategoryId === category._id)? 
+           productData.filter(product => product.supplierCategoryId === category._id).length : 0
             
+           if(productOnline > 0) {
             
             return (
               
-             <Col lg={3} md={6} >
-             <div className="single-category single-category--one space-mb--5">
-             <Card>
-              <Card.Header style={{backgroundColor : "#FBFBFF"}}><Typography variant="h4" >
-            <Box fontWeight="fontWeightBold">
-            {category.categoryName}
-              </Box>
-              </Typography>
-              <span style={{ color : "#31de79"}}>{productData.filter(product => product.supplierCategoryId === category._id)? 
-              productData.filter(product => product.supplierCategoryId === category._id).length : 0} products listed</span>
-              </Card.Header>
-              <Card.Body>
-            {/* <Card.Title>Product Count : {productData.filter(product => product.supplierCategoryId === category._id).length}</Card.Title> */}
-                
-                  <Accordion expanded={expanded === category._id || category._id === firstCategory} className={classes.root} onChange={handleChangePanel(category._id)}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1bh-content"
-            id={category._id}
-          >
-            <Typography className={classes.heading}>
-            <Box fontWeight="fontWeightBold">
-              Explore Products
-              </Box>
-              </Typography>
-            {/* <Typography className={classes.secondaryHeading}>I am an accordion</Typography> */}
-          </AccordionSummary>
-          <AccordionDetails id={category._id}>
-          <div className="cart-overlay__content-container">
-          <div className="cart-product-wrapper">
-              <div className="cart-product-container">
-              <CustomScroll allowOuterScroll={true}>
-          {productData &&
-        productData.map((product) => {
-            if(product.supplierCategoryId === category._id)
-            {
-          const discountedPrice = parseFloat(product.sellingPrice).toFixed(2);
-          const discount = (((parseFloat(product.originalPrice)-parseFloat(product.sellingPrice))/parseFloat(product.originalPrice))*100).toFixed(0);
-          const productPrice = parseFloat(product.originalPrice).toFixed(2);
-          const filteredCart = cartData.filter(cart => cart.supplierProductId === product._id);
-          
-          return(
-            <ProductActualList
-            product = {product}
-            discountedPrice = {discountedPrice}
-            discount = {discount}
-            productPrice = {productPrice}
-            filteredCart = {filteredCart}
-            />
-          )
-          
-            }
-          })}
-          </CustomScroll>
-          </div>
-          </div>
-          </div>
-          </AccordionDetails>
-        </Accordion>
-                {/* <Button size="small" variant="primary" onClick={(e) => {
-                  e.preventDefault();
-                  let string = window.location.origin + `/shop/no-sidebar/${category._id}`;
-                  window.open(string, "_self");
-                }}>Explore Products</Button> */}
-              </Card.Body>
-            </Card>
-             </div>
-            </Col>
-            )
+              <Col lg={3} md={6} >
+              <div className="single-category single-category--one space-mb--5">
+              <Card>
+               <Card.Header style={{backgroundColor : "#FBFBFF"}}><Typography variant="h4" >
+             <Box fontWeight="fontWeightBold">
+             {category.categoryName}
+               </Box>
+               </Typography>
+               <span style={{ color : "#31de79"}}>{productData.filter(product => product.supplierCategoryId === category._id)? 
+               productData.filter(product => product.supplierCategoryId === category._id).length : 0} products listed</span>
+               </Card.Header>
+               <Card.Body>
+             {/* <Card.Title>Product Count : {productData.filter(product => product.supplierCategoryId === category._id).length}</Card.Title> */}
+                 
+                   <Accordion expanded={expanded === category._id || category._id === firstCategory} className={classes.root} onChange={handleChangePanel(category._id)}>
+           <AccordionSummary
+             expandIcon={<ExpandMoreIcon />}
+             aria-controls="panel1bh-content"
+             id={category._id}
+           >
+             <Typography className={classes.heading}>
+             <Box fontWeight="fontWeightBold">
+               Explore Products
+               </Box>
+               </Typography>
+             {/* <Typography className={classes.secondaryHeading}>I am an accordion</Typography> */}
+           </AccordionSummary>
+           <AccordionDetails id={category._id}>
+           <div className="cart-overlay__content-container">
+           <div className="cart-product-wrapper">
+               <div className="cart-product-container">
+               <CustomScroll allowOuterScroll={true}>
+           {productData &&
+         productData.map((product) => {
+             if(product.supplierCategoryId === category._id)
+             {
+           const discountedPrice = parseFloat(product.sellingPrice).toFixed(2);
+           const discount = (((parseFloat(product.originalPrice)-parseFloat(product.sellingPrice))/parseFloat(product.originalPrice))*100).toFixed(0);
+           const productPrice = parseFloat(product.originalPrice).toFixed(2);
+           const filteredCart = cartData.filter(cart => cart.supplierProductId === product._id);
+           
+           return(
+             <ProductActualList
+             product = {product}
+             discountedPrice = {discountedPrice}
+             discount = {discount}
+             productPrice = {productPrice}
+             filteredCart = {filteredCart}
+             />
+           )
+           
+             }
+           })}
+           </CustomScroll>
+           </div>
+           </div>
+           </div>
+           </AccordionDetails>
+         </Accordion>
+                 {/* <Button size="small" variant="primary" onClick={(e) => {
+                   e.preventDefault();
+                   let string = window.location.origin + `/shop/no-sidebar/${category._id}`;
+                   window.open(string, "_self");
+                 }}>Explore Products</Button> */}
+               </Card.Body>
+             </Card>
+              </div>
+             </Col>
+             )
+           }
+           
           })}
                   </Row>
       </Container>
