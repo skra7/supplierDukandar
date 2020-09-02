527bee80757691a41294f782182746b7d19f64fc
Sonu Kumar <sonu0774@gmail.com>
2020-09-03 00:33:19 +0530

    changes compiled for sellerInfo header

--- src/components/Header/HeaderOne.js
+++ src/components/Header/HeaderOne.js
@@ -10,7 +10,7 @@ import {
   IoIosMenu,
   IoIosPower,
   IoLogoWhatsapp,
-  IoMdMail
+  IoMdMail,
 } from "react-icons/io";
 import { IconContext } from "react-icons";
 import Navigation from "./elements/Navigation";
@@ -19,9 +19,8 @@ import Navigation from "./elements/Navigation";
 import CartOverlay from "./elements/CartOverlay";
 // import WishlistOverlay from "./elements/WishlistOverlay";
 import MobileMenu from "./elements/MobileMenu";
-import Button from 'react-bootstrap/Button';
-import { useMediaQuery } from 'react-responsive';
-
+import Button from "react-bootstrap/Button";
+import { useMediaQuery } from "react-responsive";
 
 const HeaderOne = ({ aboutOverlay }) => {
   const [scroll, setScroll] = useState(0);
@@ -34,49 +33,57 @@ const HeaderOne = ({ aboutOverlay }) => {
   const [offCanvasMobileMenuActive, setOffCanvasMobileMenuActive] = useState(
     false
   );
-  const [mobileView , setMobileView] = useState(false);
+  const [mobileView, setMobileView] = useState(false);
   const [cartData, setCartData] = useState([]);
 
-  const [cartItem , setCartItem] = useState([]);
+  const [cartItem, setCartItem] = useState([]);
 
-  const [ loginFlag, setLoginFlag] = React.useState(true);
+  const [loginFlag, setLoginFlag] = React.useState(true);
 
-  const [supplierNumber , setSupplierNumber] = React.useState("");
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
-  const [ supplierBusiness, setSupplierBusiness] = React.useState("");;
+  const [supplierNumber, setSupplierNumber] = React.useState("");
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const [supplierBusiness, setSupplierBusiness] = React.useState("");
 
   useEffect(() => {
     const header = document.querySelector("header");
     setHeaderTop(header.offsetTop);
     setHeaderHeight(header.offsetHeight);
-    
+
     const interval = setInterval(() => {
       var cartItem = JSON.parse(localStorage.getItem("cartItem")) || [];
       var cartSupplier = localStorage.getItem("supplierId") || "";
-      var cartFinal =  cartItem.filter(cart => cart.supplierId === cartSupplier) ;
+      var cartFinal = cartItem.filter(
+        (cart) => cart.supplierId === cartSupplier
+      );
       setCartData(cartFinal);
       var sellerInfo = JSON.parse(localStorage.getItem("sellerInfo"));
-      if(sellerInfo) {
-        setSupplierBusiness(sellerInfo.shopName);
+      if (sellerInfo) {
+        var supplierData = JSON.parse(
+          localStorage.getItem("supplierBusinessDetails")
+        );
+        if (sellerInfo.shopName) {
+          setSupplierBusiness(sellerInfo.shopName);
+        } else {
+          setSupplierBusiness(supplierData.businessName);
+        }
         setSupplierNumber(sellerInfo.phoneNumber);
-      }
-      else{
-        var supplierData = JSON.parse(localStorage.getItem("supplierBusinessDetails"))
+      } else {
+        var supplierData = JSON.parse(
+          localStorage.getItem("supplierBusinessDetails")
+        );
         setSupplierBusiness(supplierData.businessName);
-      setSupplierNumber(localStorage.getItem("supplierNumber") || "");
+        setSupplierNumber(localStorage.getItem("supplierNumber") || "");
       }
-      
     }, 1000);
-    if(localStorage.getItem("userId")) {
+    if (localStorage.getItem("userId")) {
       setLoginFlag(false);
-    }
-    else {
+    } else {
       setLoginFlag(true);
     }
-    
+
     window.addEventListener("scroll", handleScroll);
-    
-      document.body.style.paddingTop = 0;
+
+    document.body.style.paddingTop = 0;
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -91,27 +98,35 @@ const HeaderOne = ({ aboutOverlay }) => {
     localStorage.removeItem("userId");
     localStorage.removeItem("userToken");
     let string = window.location.origin + "/";
-          window.open(string, "_self");
+    window.open(string, "_self");
   };
 
-
-  
   return (
-    <Fragment >
+    <Fragment>
       <header
         className={`topbar-shadow ${scroll > headerTop ? "is-sticky" : ""}`}
-        style = {isTabletOrMobile ? {background : "black"} : {background : "white"}}
+        style={
+          isTabletOrMobile ? { background: "black" } : { background: "white" }
+        }
       >
         <Container className="wide">
-          <div className="header-content d-flex align-items-center justify-content-between position-relative space-py-mobile-only--30" 
-          
-          >
+          <div className="header-content d-flex align-items-center justify-content-between position-relative space-py-mobile-only--30">
             {/* logo */}
             <div className="header-content__logo d-flex align-items-center space-pr--15">
-             
               <Link href="/" as={process.env.PUBLIC_URL + "/"}>
                 <a>
-                 <h4 style={isTabletOrMobile ? {color : "white"} : {color : "black"}}><strong>{supplierBusiness.split(" ")[0]} <span style={{ color : "#31de79"}}>{supplierBusiness.split(" ").slice(1)}</span></strong></h4>
+                  <h4
+                    style={
+                      isTabletOrMobile ? { color: "white" } : { color: "black" }
+                    }
+                  >
+                    <strong>
+                      {supplierBusiness.split(" ")[0]}{" "}
+                      <span style={{ color: "#31de79" }}>
+                        {supplierBusiness.split(" ").slice(1)}
+                      </span>
+                    </strong>
+                  </h4>
                 </a>
               </Link>
             </div>
@@ -122,7 +137,6 @@ const HeaderOne = ({ aboutOverlay }) => {
             {/* icons */}
             <div className="header-content__icons space-pl--15">
               <ul className="d-none d-lg-block">
-                
                 {/* <li>
                   <button
                     onClick={() => {
@@ -133,23 +147,23 @@ const HeaderOne = ({ aboutOverlay }) => {
                     }}
                   >
                     <IoIosHeartEmpty /> */}
-                    {/* {wishlistItems.length >= 1 ? (
+                {/* {wishlistItems.length >= 1 ? (
                       <span className="count">
                         {wishlistItems.length ? wishlistItems.length : ""}
                       </span>
                     ) : (
                       ""
                     )} */}
-                  {/* </button>
+                {/* </button>
                 </li> */}
-                <li style={{ color : "white"}}>
-                  
-                  <a  href={`https://api.whatsapp.com/send?phone=91${supplierNumber}`}
-                  target="_blank"
+                <li style={{ color: "white" }}>
+                  <a
+                    href={`https://api.whatsapp.com/send?phone=91${supplierNumber}`}
+                    target="_blank"
                   >
-                   <IoLogoWhatsapp size={30} style={{ fill: '#4FCE5D' }}/>
+                    <IoLogoWhatsapp size={30} style={{ fill: "#4FCE5D" }} />
                   </a>
-              </li>
+                </li>
                 <li>
                   <button
                     onClick={() => {
@@ -158,7 +172,9 @@ const HeaderOne = ({ aboutOverlay }) => {
                         .querySelector("body")
                         .classList.add("overflow-hidden");
                     }}
-                    style={isTabletOrMobile ? {color : "white"} : {color : "black"}}
+                    style={
+                      isTabletOrMobile ? { color: "white" } : { color: "black" }
+                    }
                   >
                     <IoIosCart />
                     {cartData.length >= 1 ? (
@@ -174,24 +190,30 @@ const HeaderOne = ({ aboutOverlay }) => {
 
               <ul className="d-block d-lg-none">
                 <li>
-                  
-                    <a  href={`https://api.whatsapp.com/send?phone=91${supplierNumber}`}
+                  <a
+                    href={`https://api.whatsapp.com/send?phone=91${supplierNumber}`}
                     target="_blank"
-                    >
-                    <IoLogoWhatsapp size={30} style={{ fill: '#4FCE5D' }}/>
-                    </a>
+                  >
+                    <IoLogoWhatsapp size={30} style={{ fill: "#4FCE5D" }} />
+                  </a>
                 </li>
                 <li>
-                <button
+                  <button
                     onClick={() => {
                       setOffCanvasCartActive(true);
                       document
                         .querySelector("body")
                         .classList.add("overflow-hidden");
                     }}
-                    style={isTabletOrMobile ? {color : "white"} : {color : "black"}}
+                    style={
+                      isTabletOrMobile ? { color: "white" } : { color: "black" }
+                    }
                   >
-                    <IoIosCart style={isTabletOrMobile ? {fill : "white"} : {fill : "black"}}/>
+                    <IoIosCart
+                      style={
+                        isTabletOrMobile ? { fill: "white" } : { fill: "black" }
+                      }
+                    />
                     {cartData.length >= 1 ? (
                       <span className="count">
                         {cartData.length ? cartData.length : ""}
@@ -201,7 +223,7 @@ const HeaderOne = ({ aboutOverlay }) => {
                     )}
                   </button>
                 </li>
-                
+
                 {/* <li>
                   <button  onClick={() => setOffCanvasMobileMenuActive(true)}>
                     <IoIosMenu style={{ fill : "white"}}/>
@@ -213,7 +235,7 @@ const HeaderOne = ({ aboutOverlay }) => {
         </Container>
       </header>
 
-     {/* about overlay */}
+      {/* about overlay */}
       {/* {aboutOverlay === false ? (
         ""
       ) : (
@@ -230,7 +252,7 @@ const HeaderOne = ({ aboutOverlay }) => {
 
       {/* cart overlay */}
       <CartOverlay
-        cartData = {cartData}
+        cartData={cartData}
         activeStatus={offCanvasCartActive}
         getActiveStatus={setOffCanvasCartActive}
       />
@@ -247,7 +269,6 @@ const HeaderOne = ({ aboutOverlay }) => {
       />
     </Fragment>
   );
-
-    }
+};
 
 export default HeaderOne;
--- src/pages/[id].js
+++ src/pages/[id].js
@@ -4,90 +4,120 @@ import CategoryTab from "../components/CategoryTab/CategoryTab";
 import { ImageCta } from "../components/Cta";
 import heroSliderData from "../data/hero-sliders/hero-slider-one.json";
 import imageCtaData from "../data/image-cta/image-cta-one.json";
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { BreadcrumbOne } from "../components/Breadcrumb";
 import Link from "next/link";
 const Home = () => {
   const router = useRouter();
   const { id } = router.query;
   const { pathname } = router.pathname;
-  const [categoryData , setCategoryData] = React.useState([]);
-  const [ supplierData, setSupplierData] = React.useState([]);
-  const [productData , setProductData] = React.useState([]);
-  const [firstCategory , setFirstCategory] = React.useState("");
+  const [categoryData, setCategoryData] = React.useState([]);
+  const [supplierData, setSupplierData] = React.useState([]);
+  const [productData, setProductData] = React.useState([]);
+  const [firstCategory, setFirstCategory] = React.useState("");
   const [cartData, setCartData] = React.useState([]);
- React.useEffect(() => {
-  
-  async function getSupplierInfo() {
-    await fetch (
-      `http://3.7.238.54:4000/seller/sellerInfoByUrl?shopUrlString=${id}`,
-      {
-        method: 'GET',
-        headers: new Headers({
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
-        })
-      }).then(r => r.json())
-      .then(r => {
-        const interval = setInterval(() => {
-          var cartItem = JSON.parse(localStorage.getItem("cartItem")) || [];
-          var cartFinal =  cartItem.filter(cart => cart.supplierId === r.data[0].userId) ;
-          setCartData(cartFinal);
-        },1000);
-        async function getCategory (){
-          await fetch (
-           `http://3.7.238.54:4000/supplierCategorybyId?id=${r.data[0].userId}`,
-           {
-             method: 'GET',
-             headers: new Headers({
-               "Content-Type": "application/json",
-               "Access-Control-Allow-Origin": "*"
-             })
-           }).then(r => r.json())
-           .then(r => {
-             setCategoryData(r.data[0])
-             setFirstCategory(r.data[0][0]._id || "")
-            })
-             
-           .catch(err =>{
-             console.log(err);
-           }
-         )
-       }
-       getCategory();
-       async function getProduct (){
-         await fetch (
-          `http://3.7.238.54:4000/supplierProductByUser?id=${r.data[0].userId}`,
-          {
-            method: 'GET',
-            headers: new Headers({
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*"
-            })
-          }).then(r => r.json())
-          .then(r => {
-            setProductData(r.data[0])})
-          .catch(err =>{
-            console.log(err);
+  React.useEffect(() => {
+    async function getSupplierInfo() {
+      await fetch(
+        `http://3.7.238.54:4000/seller/sellerInfoByUrl?shopUrlString=${id}`,
+        {
+          method: "GET",
+          headers: new Headers({
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          }),
+        }
+      )
+        .then((r) => r.json())
+        .then((r) => {
+          const interval = setInterval(() => {
+            var cartItem = JSON.parse(localStorage.getItem("cartItem")) || [];
+            var cartFinal = cartItem.filter(
+              (cart) => cart.supplierId === r.data[0].userId
+            );
+            setCartData(cartFinal);
+          }, 1000);
+          async function getUserInfo() {
+            await fetch(
+              `http://3.7.238.54:4000/supplierInfo?id=${r.data[0].userId}`,
+              {
+                method: "GET",
+                headers: new Headers({
+                  "Content-Type": "application/json",
+                  "Access-Control-Allow-Origin": "*",
+                }),
+              }
+            )
+              .then((r) => r.json())
+              .then((r) => {
+                setSupplierData(r.data.businessDetails);
+                localStorage.setItem(
+                  "supplierNumber",
+                  r.data.numberWithOutCountryCode
+                );
+                localStorage.setItem(
+                  "supplierBusinessDetails",
+                  JSON.stringify(r.data.businessDetails)
+                );
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           }
-        )
-      }
-      getProduct();
-        localStorage.setItem("sellerInfo", JSON.stringify(r.data[0]));
-        localStorage.setItem("supplierId", r.data[0].userId);
-       
-      })
-      .catch(err =>{
-        console.log(err);
-      }
-    )
-  }
-  getSupplierInfo();
-  
-  
-},[]);
-  
+          getUserInfo();
+          async function getCategory() {
+            await fetch(
+              `http://3.7.238.54:4000/supplierCategorybyId?id=${r.data[0].userId}`,
+              {
+                method: "GET",
+                headers: new Headers({
+                  "Content-Type": "application/json",
+                  "Access-Control-Allow-Origin": "*",
+                }),
+              }
+            )
+              .then((r) => r.json())
+              .then((r) => {
+                setCategoryData(r.data[0]);
+                setFirstCategory(r.data[0][0]._id || "");
+              })
+
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+          getCategory();
+          async function getProduct() {
+            await fetch(
+              `http://3.7.238.54:4000/supplierProductByUser?id=${r.data[0].userId}`,
+              {
+                method: "GET",
+                headers: new Headers({
+                  "Content-Type": "application/json",
+                  "Access-Control-Allow-Origin": "*",
+                }),
+              }
+            )
+              .then((r) => r.json())
+              .then((r) => {
+                setProductData(r.data[0]);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+          getProduct();
+          localStorage.setItem("sellerInfo", JSON.stringify(r.data[0]));
+          localStorage.setItem("supplierId", r.data[0].userId);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    getSupplierInfo();
+  }, []);
+
   return (
     <LayoutOne aboutOverlay={true}>
       {/* hero slider */}
@@ -95,19 +125,15 @@ const Home = () => {
 
       {/* product tab */}
       <CategoryTab
-      categoryData = {categoryData}
-      productData = {productData}
-      firstCategory = {firstCategory}
-      cartData = {cartData}
+        categoryData={categoryData}
+        productData={productData}
+        firstCategory={firstCategory}
+        cartData={cartData}
       />
 
       {/* image cta */}
-      
     </LayoutOne>
   );
 };
-
-
-
 
 export default Home;
